d6d8dd4f1eb5a68b508bf6a406d97db4af8fa00a
cfylover <cfylover05@gmail.com>
2024-10-19 12:21:33 +0530

    New entry contributors.js
    Added

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,4 +1,4 @@
 window.contributors = [
     // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
-    { name:"Raj", social:"xyz.com",color:red;
+    { name:"Raj", social:"https://linkedin.com/company/codebloomstudios",color:"#f26321",
     ];
