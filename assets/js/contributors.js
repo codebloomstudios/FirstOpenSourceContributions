9b964026c78d0f64bb3dece26b9dced57d8de61f
Nithin KS <50230678+nithinkodadhavadee@users.noreply.github.com>
2024-10-19 11:55:08 +0530

    Update contributors.js

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,8 +1,3 @@
 window.contributors = [
-    { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
-    { name: "Dhruva", social: "https://in.linkedin.com/in/dhruvanandan-v", color: "#f26" },
-    { name: "Krishna A L", social: "https://in.linkedin.com/in/krishna-al-8ba619166", color: "#b76" },
-    { name: "Vinodh M C", social: "https://www.cmi.ac.in/~vinodh/", color: "#f76" },
-    { name: "Nithin K S", social: "https://in.linkedin.com/in/nithinkodadhavadee", color: "#de6" },
-    { name: "ZK", social: "https://in.linkedin.com/company/zerokelvinstudio", color: "#000" },
+    // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
 ];
