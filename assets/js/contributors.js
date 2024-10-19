74907f1fb0b25cdab6d695de8a80463c735d5117
Nithin KS <50230678+nithinkodadhavadee@users.noreply.github.com>
2024-10-19 12:04:25 +0530

    New entry to contributors.js
    Added a new entry to contributors.js 
    details of Nithin KS

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,3 +1,4 @@
 window.contributors = [
     // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
+    {name: "Nithin KS", social:"https://www.linkedin.com/in/nithinkodadhavadee/", color:"#b6a3b8"},
 ];
