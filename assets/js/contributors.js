5f3d554fbad7a9433975b9ffdfa7e3c9953275e6
Ananya Ravikiran Vastare <116643029+Ananya-vastare@users.noreply.github.com>
2024-10-19 12:03:57 +0530

    contributors.js
    Added details to this repository

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,3 +1,4 @@
 window.contributors = [
     // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
+    //{ name:"Ananya Ravikiran Vastare",social :"https://www.linkedin.com/in/ananya-ravikiran-vastare-17037b250/",color:"#f2f7f7"}
 ];
