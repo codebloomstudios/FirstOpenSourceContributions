6ec11861b3489d1e452cab7ae63d8c94cfdccef8
namitha-27 <99265885+namitha-27@users.noreply.github.com>
2024-10-19 12:08:34 +0530

    Added name,social,color  contributors.js

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,3 +1,4 @@
 window.contributors = [
     // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
+    { name: "Namitha BM", social: "https://www.linkedin.com/in/namitha-bm-212b67201/", color: "#42e9f5" },
 ];
