8477b69696ee8188dee8907e12c8050fb9b7ba82
Prajwal Nayak <83334464+thech0zen1@users.noreply.github.com>
2024-10-19 12:03:23 +0530

    New Entry to  contributors.js
    Added my personal information to the contribution file.

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,3 +1,4 @@
 window.contributors = [
     // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
+    {name: "Prajwal Nayak", social:"https://www.linkedin.com/in/prajwal-nayak-64295a32b", color: "#cfff04"},
 ];
