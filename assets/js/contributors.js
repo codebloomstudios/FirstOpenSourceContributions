4270e176acf2b906655c98de30a44e216fd391c2
chiranthanHY <150021401+chiranthanHY@users.noreply.github.com>
2024-10-19 12:05:16 +0530

    new entry to  contributors.js
    added new entry

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,3 +1,4 @@
 window.contributors = [
     // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
+    {name:"Chiranthan H Y",social:"linkedin.com/in/chiranthan-h-y-62503a216",color:"#4585ed"}
 ];
