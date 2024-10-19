72af0466397837f760f52f3dc10e2d78f46029b5
Mohammed Ismail <145321352+mohammedismailmi@users.noreply.github.com>
2024-10-19 12:04:50 +0530

    New Entry to contributors.js
    Added a new entry to contributors.js
    details of Mohammed Ismail

--- assets/js/contributors.js
+++ assets/js/contributors.js
@@ -1,3 +1,4 @@
 window.contributors = [
     // { name: "Codebloom Studios", social: "https://linkedin.com/company/codebloomstudios", color: "#f26321" },
+    {Name:"Mohammed Ismail", social: "https://github.com/mohammedismailmi", colour:"#9bdb84"},
 ];
